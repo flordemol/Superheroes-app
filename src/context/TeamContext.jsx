c74442e76163b846828b92aeb27382bc24003b3f
Florencia De Mollein <florenciademollein@gmail.com>
2022-01-06 17:03:32 -0300

    Verificar cantidad de buenos y malos del equipo

--- src/context/TeamContext.jsx
+++ src/context/TeamContext.jsx
@@ -1,22 +1,48 @@
 import { createContext, useState } from 'react';
 
 export const TeamContext = createContext({
-    team: [],
+    team: {
+        members: [],
+        good: 0,
+        bad: 0,
+    },
     addTeam: () => {},
 });
 
 const { Provider } = TeamContext;
 
 export const TeamProvider = ({ children }) => {
-    const [team, setTeam] = useState([]); 
+    const [team, setTeam] = useState({
+        members: [],
+        good: 0,
+        bad: 0,
+    }); 
 
     const addTeam = (hero) => {
-       if(team.some(item => item.id === hero.id)){
+        
+
+       if(team.members.some(item => item.id === hero.id)){
             console.log("El héroe ya es parte del equipo");
         } else {
-            team.push(hero);
-            setTeam(team);
-            console.log("Héroe agregado", team)
+            if(hero.biography.alignment === "good"){
+                if(team.good < 3){
+                    team.members.push(hero);
+                    team.good++;
+                    setTeam(team);
+                    console.log("Héroe agregado", team);
+                } else {
+                    console.log("Ya hay 3 buenos en el equipo");
+                }
+            } else if(hero.biography.alignment === "bad"){
+                if(team.bad < 3){
+                    team.members.push(hero);
+                    team.bad++;
+                    setTeam(team);
+                    console.log("Héroe agregado", team);
+                } else {
+                    console.log("Ya hay 3 malos en el equipo");
+                }
+            } 
        } 
     };
 
